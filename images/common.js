b0ed7c5a66ea5c8518558d4ff21bd74f3746e5b9
younhoso <thdbsgh3443@naver.com>
2021-09-19 10:10:31 +0900

    모바일 브라우저의 높이 영역 수정

--- images/common.js
+++ images/common.js
@@ -94,9 +94,10 @@ $(function() {
 
     function setScreenSize() {
       let vh = window.innerHeight * 0.01;
-      document.querySelector('.area_sidebar').style.setProperty('--vh', `${vh}px`);
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
     setScreenSize();
+    window.addEventListener('resize', setScreenSize);
 
     Array.from((document.querySelectorAll('#container .inner_header span.date'))).forEach(function(el) {
       el.innerText = el.innerText.substr(0, 11);
