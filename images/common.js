524cca8e0600da5c1fe01d5e64f38355fbf428df
younhoso <thdbsgh3443@naver.com>
2021-10-06 22:26:04 +0900

    관리자 나의 링크 부분 연동

--- images/common.js
+++ images/common.js
@@ -1,9 +1,15 @@
 $(function() {
   var menuIcons = ['ic-html', 'ic-js', 'ic-uiux' ,'ic-etc'];
+  var social = ['ic-github', 'ic-gitbook'];
   
   $('.category_list > li a').each(function(idx, el){
     $(el).prepend(`<i class="${menuIcons[idx]}"></i>`);
   });
+
+  $('.list_sns > li a').each(function(idx, el){
+    $(el).prepend(`<i class="${social[idx]} icon_common"></i>`);
+  })
+
   $('.share_js').on('click', function(){
     $('.share_temp').addClass('on');
     setTimeout(function(){
