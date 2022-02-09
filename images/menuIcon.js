413416bb7e8541ef24a37b5c808266509b73dce7
younhoso <thdbsgh3443@naver.com>
2022-02-09 10:38:01 +0900

    메뉴 아이콘 리액트 네이티브 추가

--- images/menuIcon.js
+++ images/menuIcon.js
@@ -1,15 +1,22 @@
-$(function(){
-  var menuIcons = ['ic-html', 'ic-js', 'ic-connection', 'ic-uiux', 'ic-ts', 'ic-server'];
-  var social = ['ic-github', 'ic-gitbook'];
+$(function () {
+  var menuIcons = [
+    "ic-html",
+    "ic-js",
+    "ic-connection",
+    "ic-uiux",
+    "ic-ts",
+    "ic-react",
+    "ic-server",
+  ];
+  var social = ["ic-github", "ic-gitbook"];
 
-  $('.category_list > li > a').each(function(idx, el){
-    if($(el).hasClass('link_item')){
+  $(".category_list > li > a").each(function (idx, el) {
+    if ($(el).hasClass("link_item")) {
       $(el).prepend(`<i class="${menuIcons[idx]}"></i>`);
     }
   });
-  
-  $('.list_sns > li > a').each(function(idx, el){
+
+  $(".list_sns > li > a").each(function (idx, el) {
     $(el).prepend(`<i class="${social[idx]} icon_common"></i>`);
   });
 });
-
