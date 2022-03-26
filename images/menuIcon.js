bd57d25c4402e9b0ecb8023ae6e62be0f5d14e63
younhoso <thdbsgh3443@naver.com>
2022-03-26 12:41:35 +0900

    menuIcon 부분 수정

--- images/menuIcon.js
+++ images/menuIcon.js
@@ -1,20 +1,22 @@
 $(function () {
-  var menuIcons = [
-    "ic-html",
-    "ic-js",
-    "ic-connection",
-    "ic-uiux",
-    "ic-ts",
-    "ic-react",
-    "ic-server",
+  const menuIcons = [
+    {id: 1, name: "ic-html"},
+    {id: 2, name: "ic-js"},
+    {id: 3, name: "ic-connection"},
+    {id: 4, name: "ic-uiux"},
+    // {id: 5, name: "ic-ts"}, //타입스크립트 비공개 상태
+    {id: 6, name: "ic-react"},
+    {id: 7, name: "ic-server"},
   ];
-  var social = ["ic-github", "ic-gitbook"];
+  const social = ["ic-github", "ic-gitbook"];
 
-  $(".category_list > li > a").each(function (idx, el) {
-    if ($(el).hasClass("link_item")) {
-      $(el).prepend(`<i class="${menuIcons[idx]}"></i>`);
+  for(let i = 0; i < menuIcons.length; i++){
+    const {id, name} = menuIcons[i];
+    $(".category_list > li").eq(i).attr('data-id', id);
+    if(id === Number($(".category_list > li").eq(i).data('id'))){
+      $(".category_list > li > a").eq(i).prepend(`<i class="${name}"></i>`);
     }
-  });
+  }
 
   $(".list_sns > li > a").each(function (idx, el) {
     $(el).prepend(`<i class="${social[idx]} icon_common"></i>`);
